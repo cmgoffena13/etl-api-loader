024d631fef1a76fa4e652bb8652cf45b8085fcb7
Cortland Goffena <30168413+cmgoffena13@users.noreply.github.com>
2025-12-06 15:49:42 -0700

    adding in dummyjson

---
+++ src/sources/dummyjson/base.py
@@ -0,0 +1,23 @@
+from src.sources.base import APIConfig, APIEndpointConfig, OffsetPaginationConfig
+from src.sources.dummyjson.models.products import DummyJSONProduct
+
+DUMMYJSON_CONFIG = APIConfig(
+    base_url="https://dummyjson.com",
+    type="rest",
+    endpoints=[
+        APIEndpointConfig(
+            endpoint="/products",
+            json_entrypoint="products",
+            data_model=DummyJSONProduct,
+            pagination_strategy="offset",
+            pagination=OffsetPaginationConfig(
+                offset_param="skip",
+                limit_param="limit",
+                start_offset=0,
+                max_concurrent=5,
+                offset=0,
+                limit=10,
+            ),
+        )
+    ],
+)
---
+++ src/sources/dummyjson/models/products.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel
+from pydantic_extra_types.pendulum_dt import DateTime
+
+
+class Dimensions(BaseModel):
+    width: float
+    height: float
+    depth: float
+
+
+class Review(BaseModel):
+    rating: int
+    comment: str
+    date: DateTime
+    reviewerName: str
+    reviewerEmail: str
+
+
+class Meta(BaseModel):
+    createdAt: DateTime
+    updatedAt: DateTime
+    barcode: str
+    qrCode: str
+
+
+class DummyJSONProduct(BaseModel):
+    id: int
+    title: str
+    description: str
+    category: str
+    price: float
+    discountPercentage: float
+    rating: float
+    stock: int
+    tags: list[str]
+    brand: str
+    sku: str
+    weight: int
+    dimensions: Dimensions
+    warrantyInformation: str
+    shippingInformation: str
+    availabilityStatus: str
+    reviews: list[Review]
+    returnPolicy: str
+    minimumOrderQuantity: int
+    meta: Meta
+    images: list[str]
+    thumbnail: str
