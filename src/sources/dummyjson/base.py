082a5f762f8786eceb08f9f32aa125b03c7b8741
Cortland Goffena <30168413+cmgoffena13@users.noreply.github.com>
2025-12-06 15:51:30 -0700

    fixing config

--- src/sources/dummyjson/base.py
+++ src/sources/dummyjson/base.py
@@ -4,20 +4,20 @@ from src.sources.dummyjson.models.products import DummyJSONProduct
 DUMMYJSON_CONFIG = APIConfig(
     base_url="https://dummyjson.com",
     type="rest",
+    pagination_strategy="offset",
+    pagination=OffsetPaginationConfig(
+        offset_param="skip",
+        limit_param="limit",
+        start_offset=0,
+        max_concurrent=5,
+        offset=0,
+        limit=10,
+    ),
     endpoints=[
         APIEndpointConfig(
             endpoint="/products",
             json_entrypoint="products",
             data_model=DummyJSONProduct,
-            pagination_strategy="offset",
-            pagination=OffsetPaginationConfig(
-                offset_param="skip",
-                limit_param="limit",
-                start_offset=0,
-                max_concurrent=5,
-                offset=0,
-                limit=10,
-            ),
         )
     ],
 )
